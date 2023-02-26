f7802a88553c4ce21d4d286226e4f259381188ca
athrower3 <athrower3@gatech.edu>
2023-02-26 10:42:51 -0600

    Updated formatting on FeaturedPost

--- frontend/src/FeaturedPost.js
+++ frontend/src/FeaturedPost.js
@@ -17,18 +17,18 @@ function FeaturedPost(props) {
 			<CardActionArea component="a" href="#">
 				<Card sx={{ display: 'flex' }}>
 					<CardContent sx={{ flex: 1 }}>
-					<Typography component="h2" variant="h5">
-						{post.title}
-					</Typography>
-					<Typography variant="subtitle1" color="text.secondary">
-						{post.date}
-					</Typography>
-					<Typography variant="subtitle1" paragraph>
-						{post.description}
-					</Typography>
-					<Typography variant="subtitle1" color="primary">
-						Continue reading...
-					</Typography>
+						<Typography component="h2" variant="h5">
+							{post.title}
+						</Typography>
+						<Typography variant="subtitle1" color="text.secondary">
+							{post.date}
+						</Typography>
+						<Typography variant="subtitle1" paragraph>
+							{post.description}
+						</Typography>
+						<Typography variant="subtitle1" color="primary">
+							Continue reading...
+						</Typography>
 					</CardContent>
 					<CardMedia
 						component="img"
