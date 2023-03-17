690d70a1ca8ebc6a57e30c91cdcd4459f0e24f23
Stylianides, Stephan <s_stylianides@yahoo.com>
2023-03-16 17:35:21 -0700

    added update blog post and delete blog post routes

--- backend/controllers/blogPostsController.js
+++ backend/controllers/blogPostsController.js
@@ -1,36 +1,92 @@
 const Post = require("../dataModels/blogPostsModel");
 
-
 exports.createPost = async (req, res) => {
-    if (!req.body) {
-      return res.status(400).json({
-        message: "invalid body",
-      });
-    }
-  
-    try {
-      await Post.create({
-        title: req.body.title,
-        content: req.body.content,
-        linkToPicture: req.body.link
-        
-      });
-    } catch (err) {
-      return res.status(400).json({
-        message: err.message,
-      });
-    }
-  
-    return res.status(201).json({
-      message: "blog post created",
+  if (!req.body) {
+    return res.status(400).json({
+      message: "invalid body",
     });
-  };
+  }
+
+  try {
+    await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      linkToPicture: req.body.link,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(201).json({
+    message: "blog post created",
+  });
+};
+
+exports.getAllBlogPosts = async (req, res) => {
+  const posts = await Post.find({});
+
+  return res.status(200).json({
+    posts,
+  });
+};
 
 
-  exports.getAllBlogPosts = async (req, res) => {
-    const posts = await Post.find({});
-  
-    return res.status(200).json({
-      posts,
+
+exports.updateBlogPost = async (req, res) => {
+  const { id } = req.params;
+  let post;
+
+  if (!req.body) {
+    return res.status(400).json({
+      message: "invalid body",
     });
-  };
+  }
+
+
+  //NOTE: you must pass either title, content, and/or link
+  try {
+    post = await Post.findByIdAndUpdate(id, req.body,  {new: true});
+    if (!post) {
+      return res.status(400).json({
+        message: "Blog post ID does not exist",
+      });
+    }
+
+    
+  } catch(err) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  res.status(200).json({post});
+
+
+}
+
+exports.deleteBlogPost = async (req, res) => {
+  const { id } = req.params;
+  let post;
+
+  try {
+    post = await Post.findByIdAndDelete(id);
+    if (!post) {
+      return res.status(404).json({
+        message: "Blog post ID does not exist",
+      });
+    }
+
+    
+  } catch {
+    return res.status(404).json({
+      message: "Blog post ID does not exist",
+    });
+  }
+
+
+  return res.status(200).json({
+    message: "Blog post deleted",
+  });
+};
