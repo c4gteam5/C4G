39f15e79abd84a19904f538a2352f6f347666900
Stylianides, Stephan <s_stylianides@yahoo.com>
2023-03-25 20:07:36 -0700

    maybe this helps

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,11 @@ app.use(xss());
 //   origin: "*",
 //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 // };
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 dotenv.config({ path: "./config.env" });
 const DB = process.env.DATABASE.replace(
